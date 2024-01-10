47caf877156c6af7b2c9ab6dcd5d6bd9aed7b6e7
xiran lin <xiranlin@uw.edu>
2024-01-09 17:23:41 -0800

    2

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ st.title('Hihi')
 st.title('Here is :violet[Xiran Lin] :sunglasses:')
 
 
-
 st.subheader('About', divider='gray')
 st.text('A product designer with entrepreneurship and passion for social good and smart things.')
 
@@ -19,11 +18,3 @@ st.text('Human-Ai Interaction, Implicit Interaction, Calm Technology, Ubicomp, I
 
 col1, col2 = st.columns(2)
 col1.image('image/IMG2.JPG', caption='Xiran Lin')
-
-
-#1. A profile picture
-#2. About
-#3. Education
-#4. Work Experience
-#5. Hobbies and Interests
-#6. Interesting Projects
