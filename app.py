0a26dc094338b2f3d08770b06fbe6c60894ffe1a
xiran lin <xiranlin@uw.edu>
2024-01-09 17:16:43 -0800

    2

--- app.py
+++ app.py
@@ -1,6 +1,29 @@
 import streamlit as st
 
+st.title('Hihi')
+st.title('Here is :violet[Xiran Lin] :sunglasses:')
 
-st.title('Xiran Lin')
-st.title('Bout')
-st.title('_Xiran_ is :blue[cool] :sunglasses:')
+
+
+st.subheader('About', divider='gray')
+st.text('A product designer with entrepreneurship and passion for social good and smart things.')
+
+st.subheader('Education')
+st.text('B.A in Industrial Design')
+st.subheader('Work Experience')
+st.text('Work in Fabrie,Envision Digital,Shenzhen InnoX Academy,Midea Real Estate')
+st.subheader('Hobbies and Interests')
+st.text('Badminton, Hiking, Drama, Movie, Chinese Poem, Table Games')
+st.subheader('Interests')
+st.text('Human-Ai Interaction, Implicit Interaction, Calm Technology, Ubicomp, Inclusive Design')
+
+col1, col2 = st.columns(2)
+col1.image('image/IMG2.JPG', caption='Xiran Lin')
+
+
+#1. A profile picture
+#2. About
+#3. Education
+#4. Work Experience
+#5. Hobbies and Interests
+#6. Interesting Projects
